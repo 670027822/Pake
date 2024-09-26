ece3597c24823472057bdebf3f63a6d98e1f2cdc
liuhm <63099648+670027822@users.noreply.github.com>
2024-09-26 19:40:15 +0800

    Update custom.js
    Signed-off-by: liuhm <63099648+670027822@users.noreply.github.com>

--- src-tauri/src/inject/custom.js
+++ src-tauri/src/inject/custom.js
@@ -6,12 +6,13 @@
  */
 
 
+
 setInterval(() => {
 	
 console.log('start');
 	
 // 指定要隐藏的 class 名称
-const classNamesToHide = ['cursor-pointer.logo_box', 'mt-auto.text-xs', 'fixed.right-1.bottom-20.md\\:right-6.md\\:bottom-4.z-50'];
+const classNamesToHide = ['flex.items-center.gap-x-12', 'w-full.md\\:h-96.lg\\:overflow-y-auto.p-4.box-border.lg\\:col-span-7.flex.items-center.justify-center.relative','inline-flex.items-center.justify-center.rounded-md.px-2.text-gray-700','min-w-20.px-2.py-1\\.5.rounded.text-xs.text-gray-400.bg-white\\/10.cursor-not-allowed.transform.duration-500.group.inline-flex.items-center.justify-center.gap-1'];
 
 // 遍历所有需要隐藏的 class 名称
 classNamesToHide.forEach(className => {
@@ -22,25 +23,29 @@ classNamesToHide.forEach(className => {
         element.style.display = 'none';
     });
 });
+
 	
-//隐藏相邻元素
-	
-  const targetElement = document.querySelector('.px-2.w-full.h-6.overflow-hidden.cursor-pointer');
-  if (targetElement) {
-    targetElement.style.display = 'none';
-    let sibling = targetElement.previousElementSibling;
-    for (let i = 0; i < 2 && sibling; i++) {
-      sibling.style.display = 'none';
-      sibling = sibling.previousElementSibling;
-    }
-  }
 
 //移除监听事件
 	
-const avatarElement = document.querySelector('.user-avatar');
+const avatarElement = document.querySelector('.rounded-full.cursor-pointer');
 avatarElement.outerHTML = avatarElement.outerHTML;
 	
+// 获取具有指定class的元素
+var elements = document.querySelectorAll('.w-full.p-3.mb-2.border-b.border-gray-100.text-xs.text-zinc-900.flex.flex-col.items-start.gap-2');
 
+// 遍历所有找到的元素
+elements.forEach(function(element) {
+    // 获取之后的相邻两个元素
+    var nextElement = element.nextElementSibling;
+    if (nextElement) {
+        nextElement.style.display = 'none'; // 隐藏第一个相邻元素
+        var nextNextElement = nextElement.nextElementSibling;
+        if (nextNextElement) {
+            nextNextElement.style.display = 'none'; // 隐藏第二个相邻元素
+        }
+    }
+});
 	
 	
 }, 200);
